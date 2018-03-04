444b9425896c739a4ef2b4ad1c19b55b161bb1cf
LAPTOP-2G4LLGDS\User <davidroz@kth.se>
2018-03-03 20:11:38 -0600

    First version of data counting function, count packets and prints of the number of each packets. Included debug feature, to en/de printout in terminal.

--- main.py
+++ main.py
@@ -6,26 +6,85 @@
 import sys, getopt, os
 from numpy import *
 
+debug = True
+
+#Constants
+PACKET_SIZE = 8                                                     #Length of each packets in bytes
+
 file = "Test_Data/D_00032"
+packetList = []
+
 
 def read_data():
-    f = open(file, "rb")
-    st = os.stat(file)
-    block = f.read(st.st_size)
-    print(block)
-    str = ""
-    count = 8
-    for ch in block:
-        str += hex(ch)[2:].zfill(2).upper()
-        count = count - 1
-        if count == 0:
-            str += '\n'
-            count = 8
-    print(str)
+    if debug:
+        print('Start reading in data')
+        print('Opening File:', file)
 
+    f = open(file, "rb")                                            #Open file
+    st = os.stat(file)                                              #Get file size
+
+    if debug: print('File Size: ', st.st_size)
+
+    block = f.read(st.st_size)                                      #Read in blocks of data
+
+    str = ""
+    count = PACKET_SIZE
+    packet = ''
+    #TODO: Fix formtting if data is not align correctly
+    for ch in block:
+        packet += hex(ch)[2:].zfill(2).upper()
+        count -= 1
+        if count == 0:
+            str += packet
+            packetList.append(packet)
+            str += '\n'
+            packet = ''
+            count = PACKET_SIZE
+
+    if debug:
+        print('Number of packets read in: ', packetList.__len__())
+        print('Data read in done')
+        print('--------------------------------------')
 
 def count_packages():
-    pass
+    if debug:
+        print('Start counting packets')
+
+    nrADC = 0
+    nrACC = 0
+    nrGyro = 0
+    nrTime = 0
+
+    for packet in packetList:
+        if packet[:4] == '01AD':
+            nrADC += 1
+        elif packet[:4] == '02AD':
+            nrADC += 1
+        elif packet[:4] == '03AD':
+            nrADC += 1
+        elif packet[:3] == '2AC':
+            nrACC += 1
+        elif packet[:3] == '3AC':
+            nrACC += 1
+        elif packet[:4] == '0C0C':
+            nrGyro += 1
+        elif packet[:4] == '0C0B':
+            nrGyro += 1
+        elif packet[:4] == '4006':
+            nrTime+= 1
+        else:
+            print('ERROR! Mismatched packet!:', packet)
+
+    if debug:
+        print('Number of matched ADC packets:  ', nrADC)
+        print('Number of matched ACC packets:  ', nrACC)
+        print('Number of matched Gyro packets: ', nrGyro)
+        print('Number of matched Time packets: ', nrTime)
+        tot = nrADC + nrACC + nrGyro + nrTime
+        print('Total number of matched packets: ', tot)
+        print('--------------------------------------')
+
+
 
 
 def present_results():
