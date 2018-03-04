398aa2d821963f76ec3d7de777512bfc8bc8f7f2
LAPTOP-2G4LLGDS\User <davidroz@kth.se>
2018-03-03 18:30:50 -0600

    First version of data read function, format data into strings with width of 8 bytes, prints each 8 bytes.

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+##
+# WOLF CU SD-Card Data Reader
+# Description:
+# Author: David Rozenbeek
+
+import sys, getopt, os
+from numpy import *
+
+file = "Test_Data/D_00032"
+
+def read_data():
+    f = open(file, "rb")
+    st = os.stat(file)
+    block = f.read(st.st_size)
+    print(block)
+    str = ""
+    count = 8
+    for ch in block:
+        str += hex(ch)[2:].zfill(2).upper()
+        count = count - 1
+        if count == 0:
+            str += '\n'
+            count = 8
+    print(str)
 
 
+def count_packages():
+    pass
 
+
+def present_results():
+    pass
+
+
+def main():
+
+    print("------------ WOLF CU SD-CARD Data reader ------------")
+
+    # TODO: Import files and store data
+    read_data()
+
+    # TODO: Count packets
+    count_packages()
+
+    # TODO: Present results
+    present_results()
+
+
+main()
